ff76c21d666ff3df1f70a779413fb97dd45e51cd
khushi sharma <mekhushi0709@gmail.com>
2026-01-30 08:53:28 +0530

    Lottery-Ticket

---
+++ src/Helper.js
@@ -0,0 +1,12 @@
+function genTicket(n){
+    let arr=new Array(n);
+    for(let i=0;i<n;i++){
+        arr[i]=Math.floor(Math.random()*10);
+    }
+    return arr;
+}
+
+function sum(arr){
+    return arr.reduce((sum,current)=>sum+current,0); 
+}
+export {genTicket,sum};
